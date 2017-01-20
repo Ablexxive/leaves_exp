098dafc91723f2b96b1ec4f172dfcd2082bfe7b0
Sahil Khanna <sahil@fomoro.com>
2017-01-20 15:21:53 -0800

    Added Sacred to main.py
    Replaced tf FLAGS with sacred configs.

--- leaves_exp/main.py
+++ leaves_exp/main.py
@@ -9,37 +9,66 @@ import random
 import numpy as np
 import tensorflow as tf
 
+from sacred import Experiment
+
 from model import model_fn
 from dataset import Dataset
 from data_utils import load_data
 
-tf.logging.set_verbosity(tf.logging.INFO)
-
+#tf.logging.set_verbosity(tf.logging.INFO)
+ex = Experiment('sacred_test')
 LOGS_DIRECTORY = os.environ.get('LOGS_DIRECTORY', 'logs/')
 DATA_DIRECTORY = os.environ.get('DATA_DIRECTORY', './datasets/')
 
-FLAGS = tf.app.flags.FLAGS
+
+@ex.config
+def config():
+    #move flags here
+    seed = 42
+    model_dir = 'logs/'
+    dataset = './datasets/train.csv'
+    num_epochs = 200
+    learning_rate = 1e-3
+    learning_rate_decay_rate = 0.1
+    learning_rate_decay_steps = 6000
+    early_stopping_rounds = 10
+
+    flags = {
+        'seed':seed,
+        'model_dir':model_dir,
+        'dataset':dataset,
+        'num_epochs':200,
+        'learning_rate':learning_rate,
+        'learning_rate_decay_rate':learning_rate_decay_rate,
+        'learning_rate_decay_steps':6000,
+        'early_stopping_rounds':10,
+    }
+
+#FLAGS = tf.app.flags.FLAGS
 
 #Create Flags here as needed
 #tf.app.flags.DEFINE_integer/float/string/boolean('flag_name', default, 'info')
-tf.app.flags.DEFINE_integer('seed', 42, 'Random seed.')
-tf.app.flags.DEFINE_string('model_dir', 'logs/', 'Model directory.')
-tf.app.flags.DEFINE_string('dataset', './datasets/train.csv', 'Path of dataset.')
+#tf.app.flags.DEFINE_integer('seed', 42, 'Random seed.')
+#tf.app.flags.DEFINE_string('model_dir', 'logs/', 'Model directory.')
+#tf.app.flags.DEFINE_string('dataset', './datasets/train.csv', 'Path of dataset.')
 
-tf.app.flags.DEFINE_integer('num_epochs', 200, 'Number of training epochs.')
-tf.app.flags.DEFINE_float('learning_rate', 1e-3, 'Learning rate.')
-tf.app.flags.DEFINE_float('learning_rate_decay_rate', 0.1, 'Learning rate decay rate.')
-tf.app.flags.DEFINE_float('learning_rate_decay_steps', 6000, 'Learning rate decay steps.')
-tf.app.flags.DEFINE_integer('early_stopping_rounds', 10, 'Number of epochs before early stopping.')
+#tf.app.flags.DEFINE_integer('num_epochs', 200, 'Number of training epochs.')
+#tf.app.flags.DEFINE_float('learning_rate', 1e-3, 'Learning rate.')
+#tf.app.flags.DEFINE_float('learning_rate_decay_rate', 0.1, 'Learning rate decay rate.')
+#tf.app.flags.DEFINE_float('learning_rate_decay_steps', 6000, 'Learning rate decay steps.')
+#tf.app.flags.DEFINE_integer('early_stopping_rounds', 10, 'Number of epochs before early stopping.')
 
-def main(_):
-    random.seed(FLAGS.seed)
-    np.random.seed(FLAGS.seed)
+#@ex.automain
+#def run(flags):
+@ex.automain
+def main(flags):
+    random.seed(flags["seed"])
+    np.random.seed(flags["seed"])
 
     dataset = Dataset(DATA_DIRECTORY, 32, 1000)
     # both train/eval input_fn's are built off of the training data just so we have the pipleine tested.
     train_input_fn = dataset.get_input_fn('train.csv',
-        num_epochs=FLAGS.num_epochs,
+        num_epochs=flags["num_epochs"],
         shuffle=True)
     eval_input_fn = dataset.get_input_fn('train.csv',
         num_epochs=1,
@@ -52,9 +81,9 @@ def main(_):
     print("Labels!")
     print(labels) 
     params = {
-        'learning_rate': FLAGS.learning_rate,
-        'learning_rate_decay_rate': FLAGS.learning_rate_decay_rate,
-        'learning_rate_decay_steps': FLAGS.learning_rate_decay_steps,
+        'learning_rate': flags["learning_rate"],
+        'learning_rate_decay_rate': flags["learning_rate_decay_rate"],
+        'learning_rate_decay_steps': flags["learning_rate_decay_steps"],
     } #Non learned parameters for the model such as learning rate
     
     train_monitors = [
@@ -64,7 +93,7 @@ def main(_):
         tf.contrib.learn.monitors.ValidationMonitor(
             input_fn=train_input_fn,
             every_n_steps=dataset.steps_per_epoch,
-            early_stopping_rounds=FLAGS.early_stopping_rounds * dataset.steps_per_epoch,
+            early_stopping_rounds=flags["early_stopping_rounds"] * dataset.steps_per_epoch,
             early_stopping_metric='loss',
             early_stopping_metric_minimize=True)
     ]
@@ -77,16 +106,16 @@ def main(_):
     
     #Config file for how the model is saved/logged
     config = tf.contrib.learn.RunConfig(
-        tf_random_seed=FLAGS.seed,
+        tf_random_seed=flags["seed"],
         save_summary_steps=120,#how often the model is evaluated?
         save_checkpoints_secs=600,#how often the model is saved
         keep_checkpoint_max=5,
         keep_checkpoint_every_n_hours=1,
         log_device_placement=True) #?
 
-    dataset_name = os.path.splitext(os.path.basename(FLAGS.dataset))[0]
+    dataset_name = os.path.splitext(os.path.basename(flags["dataset"]))[0]
     timestamp = int(time.time())
-    model_dir = os.path.join(FLAGS.model_dir, dataset_name, str(timestamp))
+    model_dir = os.path.join(flags["model_dir"], dataset_name, str(timestamp))
     estimator = tf.contrib.learn.Estimator(
         model_dir=model_dir,
         model_fn=model_fn,
@@ -105,5 +134,3 @@ def main(_):
 
     experiment.train_and_evaluate()
 
-if __name__ == '__main__':
-    tf.app.run()
--- sacred_test.py
+++ sacred_test.py
@@ -4,14 +4,22 @@ ex = Experiment('config_test')
 
 @ex.config
 def cfg():
+    C = 10.0
+    gamma = 0.7
     params = {
-        'C':1.0,
-        'gamma':0.7,
+        'C':C,
+        'gamma':gamma,
         'learning_rate':10,
         'file_dir':"testing.csv",
     }
 
+@ex.named_config
+def variant1():
+    C=100000
+    params = {}
+
 @ex.automain
 def run(params):
+    print(type(params['C']))
     print(params)
     return params['C']
